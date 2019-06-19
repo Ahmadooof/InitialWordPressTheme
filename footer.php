345d3e497f3350e877a3bd059860ef7c9e82d2ea
ahmadooof <aa224rm@student.lnu.se>
2019-06-19 12:52:12 +0200

    adding menu feature

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+    <div class="footer text-center">
+        This is a footer.
+    </div>
+
+    <?php wp_footer(); ?>
+    </body>
+</head>
--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@
     function add_styles(){
         wp_enqueue_style('bootstrap-css',get_template_directory_uri().'/css/bootstrap.min.css');
         wp_enqueue_style('fontAwesome-css',get_template_directory_uri().'/css/font-awesome.min.css');
+        wp_enqueue_style('main-css',get_template_directory_uri().'/css/main.css');
     }
 
     /*
@@ -13,7 +14,11 @@
     ** @param true, just to load these files in footer.
     */
     function add_scripts(){
-        wp_enqueue_script('bootstrap-js',get_template_directory_uri().'/js/bootstrap.min.js',array(),false,true);
+
+        wp_deregister_script('jquery');         // remove default regerstration of jquery by wordpress.
+        wp_register_script('jquery', includes_url('/js/jquery/jquery.js'), false, '', true);  // adding jquery in the body.
+        wp_enqueue_script('jquery');  
+        wp_enqueue_script('bootstrap-js',get_template_directory_uri().'/js/bootstrap.min.js',array('jquery'),false,true);
         wp_enqueue_script('main-js',get_template_directory_uri().'/js/main.js',array(),false,true);
     }
 
@@ -25,5 +30,21 @@
 
     add_action('wp_enqueue_scripts','add_styles');
     add_action('wp_enqueue_scripts','add_scripts');
+    add_action('init', 'register_custom_menu');
+
+    /*
+    ** 
+    ** Adding custom menu feature to our theme
+    */
+    function register_custom_menu(){
+        register_nav_menu('bootstrap-nav-menu', __('Navigation Bar'));
+    }
+
+    wp_nav_menu();
+
+    /*
+    ** 
+    ** Adding custom menu feature to our theme
+    */
 
     ?>
